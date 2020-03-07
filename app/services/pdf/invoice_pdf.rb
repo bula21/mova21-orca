d2ece8c927bbf5d0c9f4e4ea09bd0a9ba2c33d35
Diego Steiner <diego.steiner@u041.ch>
2020-03-07 15:40:48 +0100

    Fix invoice specs

--- app/services/pdf/invoice_pdf.rb
+++ app/services/pdf/invoice_pdf.rb
@@ -3,7 +3,6 @@ require 'prawn'
 module Pdf
   class InvoicePdf < BasePdf
     attr_reader :invoice
-    delegate :booking, :organisation, to: :invoice
 
     def initialize(invoice)
       super()
@@ -15,21 +14,21 @@ module Pdf
       ocr_font_path = File.join(FONTS_PATH, 'ocrb', 'webfonts', 'OCR-B-regular-web.ttf')
       @document.font_families.update('ocr' => { normal: ocr_font_path })
     end
-  end
 
-  def render
-    render_recipient_address
-    text "Hello"
-    super
-  end
+    def render
+      render_recipient_address
+      super
+    end
 
-  def render_recipient_address
-    bounding_box [300, 690], width: 200, height: 140 do
-      default_leading 4
-      text Unit.model_name.human(count: :one), size: 13, style: :bold
-      move_down 5
-      text @invoice.unit.name, size: 9
-      text @invoice.invoice_address, size: 11
+    def render_recipient_address
+      bounding_box [300, 690], width: 200, height: 140 do
+        default_leading 4
+        text Unit.model_name.human(count: :one), size: 13, style: :bold
+        move_down 5
+        text "Hello"
+        text @invoice.unit.title, size: 9
+        text @invoice.invoice_address, size: 11
+      end
     end
   end
 end
--- spec/services/pdf/invoice_pdf_spec.rb
+++ spec/services/pdf/invoice_pdf_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Pdf::InvoicePdf do
   let(:invoice) { create(:invoice) }
   let(:pdf) { described_class.new(invoice) }
 
-  subject(:inspector) { PDF::Inspector::Text.analyze(pdf.render) }
+  subject(:rendered) { pdf.render }
+  subject(:inspector) { PDF::Inspector::Text.analyze(rendered) }
 
   it { expect(inspector.strings.join).to include("Hello") }
+  it do
+    File.open("/app/tmp/invoice.pdf", "wb") do |file|
+      file.write(pdf.render)
+    end
+  end
+
 end
