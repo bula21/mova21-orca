cc3274159193541bcb65acd4049f91350c3147e6
Diego Steiner <939106+diegosteiner@users.noreply.github.com>
2020-05-19 12:54:48 +0200

    Update db/migrate/20200404085605_create_activities.rb
    Co-authored-by: Simon Huber <simon.huber@renuo.ch>

--- db/migrate/20200404085605_create_activities.rb
+++ db/migrate/20200404085605_create_activities.rb
@@ -1,7 +1,7 @@
 class CreateActivities < ActiveRecord::Migration[6.0]
   def change
     create_table :activities do |t|
-      t.string :label
+      t.string :label, null: false
       t.text :description
       t.string :language
       t.string :js_type
