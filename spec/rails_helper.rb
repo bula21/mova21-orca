1106c325ecb458c04910498ff65b265f22dee647
Diego Steiner <diego.steiner@u041.ch>
2020-06-06 18:40:08 +0200

    Fix specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  # config.default_cassette_options = { record: :all }
 end
 
 RSpec.configure do |config|
