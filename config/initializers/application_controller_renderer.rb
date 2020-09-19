5bd1ce4a3ea64a386e6b37fb94a93ad73c51d36a
Diego Steiner <diego.steiner@u041.ch>
2020-09-19 16:11:40 +0200

    Fix APP_HOST

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -2,8 +2,8 @@
 
 # Be sure to restart your server when you modify this file.
 
-ActiveSupport::Reloader.to_prepare do
-  ApplicationController.renderer.defaults.merge!(
-    http_host: ENV.fetch('APP_HOST')
-  )
-end
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: ENV.fetch('APP_HOST')
+#   )
+# end
