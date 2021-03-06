7830920e9eae1c8a5b5c3b10715960d4fac3fcbf
Emily Wangler <em.wangler@gmail.com>
2021-03-06 17:56:06 +0100

    add destroy button and some translations

--- app/controllers/concerns/admin/activity_categories_controller.rb
+++ app/controllers/concerns/admin/activity_categories_controller.rb
@@ -16,7 +16,7 @@ module Admin
       @activity_category = ActivityCategory.new(activity_category_params)
 
       if @activity_category.save
-        redirect_to admin_activity_categories_path, notice: 'Activity Category was successfully created.'
+        redirect_to admin_activity_categories_path, notice: I18n.t('messages.created.success')
       else
         render :new
       end
@@ -24,7 +24,7 @@ module Admin
 
     def update
       if @activity_category.update(activity_category_params)
-        redirect_to admin_activity_categories_path, notice: 'Activity Category was successfully updated.'
+        redirect_to admin_activity_categories_path, notice: I18n.t('messages.updated.success')
       else
         render :edit
       end
@@ -32,7 +32,7 @@ module Admin
 
     def destroy
       @activity_category.destroy
-      redirect_to admin_activity_categories_url, notice: 'Activity Category was successfully destroyed.'
+      redirect_to admin_activity_categories_url, notice: I18n.t('messages.deleted.success')
     end
 
     private
