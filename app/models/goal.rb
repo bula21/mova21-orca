3c906b016deb39929231ae189d19fb7fb55e88e9
Simon Huber <simon.huber@renuo.ch>
2021-09-18 17:06:41 +0200

    Change goal name to translated field

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -10,7 +10,10 @@
 #  updated_at :datetime         not null
 #
 class Goal < ApplicationRecord
+  extend Mobility
+
   has_and_belongs_to_many :activities
 
   validates :name, presence: true
+  translates :name, type: :string, locale_accessors: true, fallbacks: true
 end
