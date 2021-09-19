3a0c482c423fbd90b6b81167e17a9d53d52bfd9c
Simon Huber <simon.huber@renuo.ch>
2021-09-19 11:49:14 +0200

    Satisfy linter

--- app/mailers/camp_unit_mailer.rb
+++ app/mailers/camp_unit_mailer.rb
@@ -5,7 +5,9 @@ class CampUnitMailer < ApplicationMailer
     @camp_unit = Unit.find params[:camp_unit_id]
     @lagerleiter = @camp_unit.lagerleiter
 
-    mail(to: @lagerleiter.email, subject: I18n.t('camp_unit_mailer.incomplete_notification.subject'))
+    I18n.with_locale(@camp_unit.language || @lagerleiter.language || I18n.default_locale) do
+      mail(to: @lagerleiter.email, subject: I18n.t('camp_unit_mailer.incomplete_notification.subject'))
+    end
   end
 
   def complete_notification
