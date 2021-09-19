4f4e4f7e3d47e961f5915b0ccffd88ded3210e29
Simon Huber <simon.huber@renuo.ch>
2021-09-19 11:45:30 +0200

    Translate email

--- app/mailers/camp_unit_mailer.rb
+++ app/mailers/camp_unit_mailer.rb
@@ -12,6 +12,8 @@ class CampUnitMailer < ApplicationMailer
     @camp_unit = Unit.find params[:camp_unit_id]
     @lagerleiter = @camp_unit.lagerleiter
 
-    mail(to: @lagerleiter.email, subject: I18n.t('camp_unit_mailer.complete_notification.subject'))
+    I18n.with_locale(@camp_unit.language || @lagerleiter.language || I18n.default_locale) do
+      mail(to: @lagerleiter.email, subject: I18n.t('camp_unit_mailer.complete_notification.subject'))
+    end
   end
 end
