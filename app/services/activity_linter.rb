713b5d0eb658c62d18eabf77e1f51f1d9dd44419
Diego Steiner <diego.steiner@u041.ch>
2021-09-23 15:27:06 +0000

    Fix linting issues

--- app/services/activity_linter.rb
+++ app/services/activity_linter.rb
@@ -1,11 +1,13 @@
-class ActivityLinter 
+# frozen_string_literal: true
+
+class ActivityLinter
   CHECKS = {
-      title_translated: ->(activity) { activity.label_in_database.values.count(&:present?) > 3 },
-      description_translated: ->(activity) { activity.description_in_database.values.count(&:present?) > 3 },
-      participant_count_min: ->(activity) { activity.participants_count_activity&.>=(12) },
-      valid: ->(activity) { activity.valid? },
-      languages_min: ->(activity) { activity.languages.values.count(&:itself) >= 1 },
-      stufen_min: ->(activity) { (activity.stufe_ids & activity.stufe_recommended_ids).count >= 1 },
+    title_translated: ->(activity) { activity.label_in_database.values.count(&:present?) > 3 },
+    description_translated: ->(activity) { activity.description_in_database.values.count(&:present?) > 3 },
+    participant_count_min: ->(activity) { activity.participants_count_activity&.>=(12) },
+    valid: ->(activity) { activity.valid? },
+    languages_min: ->(activity) { activity.languages.values.count(&:itself) >= 1 },
+    stufen_min: ->(activity) { (activity.stufe_ids & activity.stufe_recommended_ids).count >= 1 }
   }.freeze
 
   def lint(activities = Activity.all)
