927ac3e4cb46ebd2dd0235005dfe43cfffbebb5f
Diego Steiner <diego.steiner@u041.ch>
2021-09-30 23:13:44 +0000

    Fix the f*cking f*ck

---
+++ config/initializers/httpclient.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# See https://github.com/nahi/httpclient/issues/445
+require 'httpclient'
+
+class HTTPClient
+  alias original_initialize initialize
+
+  def initialize(...)
+    original_initialize(...)
+    # Force use of the default system CA certs (instead of the 6 year old bundled ones)
+    @session_manager&.ssl_config&.set_default_paths
+  end
+end
