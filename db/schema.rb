4d924dba345d81a3ae63c885e44d0d2ba2d7e2b3
Diego Steiner <diego.steiner@u041.ch>
2021-10-13 20:14:58 +0000

    Update active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_185706) do
+ActiveRecord::Schema.define(version: 2021_10_13_201436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
