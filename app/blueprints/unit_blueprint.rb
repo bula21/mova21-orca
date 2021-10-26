7f638000376a7c192af97bb9799435b9da9e3501
Diego Steiner <diego.steiner@u041.ch>
2021-10-26 13:16:58 +0200

    Remove unit_activities from unit endpoint

--- app/blueprints/unit_blueprint.rb
+++ app/blueprints/unit_blueprint.rb
@@ -2,7 +2,6 @@
 
 class UnitBlueprint < Blueprinter::Base
   identifier :id
-  association :unit_activities, blueprint: UnitActivityBlueprint
 
   fields :language, :stufe, :week, :starts_at, :ends_at, :participant_role_counts, :district
 end
