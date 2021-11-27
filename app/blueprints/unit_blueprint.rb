af18950ffdbc4263f2f1e900e35512c271d10c33
Simon Huber <simon.huber@renuo.ch>
2021-11-27 11:31:27 +0100

    Extend blueprints

--- app/blueprints/unit_blueprint.rb
+++ app/blueprints/unit_blueprint.rb
@@ -8,4 +8,18 @@ class UnitBlueprint < Blueprinter::Base
   view :with_unit_activities do
     association :unit_activities, blueprint: UnitActivityBlueprint
   end
+
+  field :week_nr do |unit|
+    # since this was hardcoded
+    case unit.week
+    when /Erste|Première|Prima/
+      1
+    when /Zweite|Deuxième|Seconda/
+      2
+    end
+  end
+
+  field :district_nr do |unit|
+    unit.district&.scan(/\d*/)&.join('')&.to_i
+  end
 end
