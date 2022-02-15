a0547bc645f02ad12e030bc8b6eac7203713d493
Simon Huber <simon.huber@renuo.ch>
2022-02-15 06:52:04 +0100

    Add Stufe to FixedEvents (#155)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_105036) do
+ActiveRecord::Schema.define(version: 2022_02_14_203715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,13 @@ ActiveRecord::Schema.define(version: 2022_02_05_105036) do
     t.datetime "ends_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fixed_events_stufen", force: :cascade do |t|
+    t.bigint "fixed_event_id", null: false
+    t.bigint "stufe_id", null: false
+    t.index ["fixed_event_id"], name: "index_fixed_events_stufen_on_fixed_event_id"
+    t.index ["stufe_id"], name: "index_fixed_events_stufen_on_stufe_id"
   end
 
   create_table "goals", force: :cascade do |t|
@@ -353,6 +360,8 @@ ActiveRecord::Schema.define(version: 2022_02_05_105036) do
   add_foreign_key "activity_executions", "activities"
   add_foreign_key "activity_executions", "fields"
   add_foreign_key "fields", "spots"
+  add_foreign_key "fixed_events_stufen", "fixed_events"
+  add_foreign_key "fixed_events_stufen", "stufen"
   add_foreign_key "invoice_parts", "invoices"
   add_foreign_key "invoices", "units"
   add_foreign_key "participant_units", "participants"
