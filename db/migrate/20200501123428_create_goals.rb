8d2ea35f40602151cf19ff97e113714eb7a6d9db
Diego Steiner <diego.steiner@u041.ch>
2022-02-23 22:00:55 +0100

    fix: ci setup

--- db/migrate/20200501123428_create_goals.rb
+++ db/migrate/20200501123428_create_goals.rb
@@ -5,11 +5,5 @@ class CreateGoals < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
-
-    Goal.create(name: 'Pfadi (er) leben')
-    Goal.create(name: 'Diversität und Inklusion')
-    Goal.create(name: 'Umwelt')
-    Goal.create(name: 'Prävention')
-    Goal.create(name: 'Dimension und Austausch')
   end
 end
