c3e1a5b9b7667e24dd0a93f54bb9f90e33fc620b
Diego Steiner <diego.steiner@u041.ch>
2022-02-24 09:47:19 +0000

    fix: failing specs

--- spec/factories/fixed_events.rb
+++ spec/factories/fixed_events.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     title { 'Lagereröffnung' }
     starts_at { Faker::Time.between(from: 2.days.ago, to: Time.zone.today) }
     ends_at { starts_at + (1..3).to_a.sample.hours }
+    stufen { Stufe.all.count > 2 ? Stufe.all.sample(2) : create_list(:stufe, 2) }
   end
 end
