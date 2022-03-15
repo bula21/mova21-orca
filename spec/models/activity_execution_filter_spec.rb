abbc94c6c7211a340fac7e7ee5472026dec8a6a9
Diego Steiner <diego.steiner@u041.ch>
2022-03-15 15:00:48 +0000

    fix: add specs

---
+++ spec/models/activity_execution_filter_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ActivityExecutionFilter, type: :model do
+  subject(:filter) { described_class.new(**params) }
+
+  let(:activity) { create(:activity, participants_count_activity: 50) }
+  let(:params) { {} }
+
+  describe '#apply' do
+    subject { filter.apply(ActivityExecution.all) }
+
+    let!(:activity_executions) do
+      create_list(:activity_execution, 3, activity: activity)
+    end
+
+    context 'with no params' do
+      it { is_expected.to include(*activity_executions) }
+    end
+
+    context 'with field' do
+      let(:field) { create(:field) }
+      let(:params) { { field: field.id } }
+      let!(:activity_executions_on_field) { create_list(:activity_execution, 2, activity: activity, field: field) }
+
+      it { is_expected.to include(*activity_executions_on_field) }
+    end
+  end
+end
