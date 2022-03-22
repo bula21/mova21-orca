76a477b811213f99a7ee7a11363e7a8780333f95
Diego Steiner <diego.steiner@u041.ch>
2022-03-22 17:55:55 +0100

    fix: remove broken migration

--- db/migrate/20220113194050_add_role_to_participant_units.rb
+++ db/migrate/20220113194050_add_role_to_participant_units.rb
@@ -2,12 +2,12 @@ class AddRoleToParticipantUnits < ActiveRecord::Migration[6.0]
   def up
     add_column :participant_units, :role, :string, null: true
 
-    ParticipantUnit.reset_column_information
-    ParticipantUnit.find_each do |participation|
-      participation.update(role: participation.participant.role) if participation.role.blank?
-    end
+    # ParticipantUnit.reset_column_information
+    # ParticipantUnit.find_each do |participation|
+    #   participation.update(role: participation.participant.role) if participation.role.blank?
+    # end
 
-    remove_column :participants, :role, :string, null: true
+    # remove_column :participants, :role, :string, null: true
   end
 
   def down
