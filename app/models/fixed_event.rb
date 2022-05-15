d9ee8f255c45a8be11e9ce75ccbeb384da9c4290
Simon Huber <simon.huber@renuo.ch>
2022-05-15 18:11:49 +0200

    Add documents to fixed_events

--- app/models/fixed_event.rb
+++ app/models/fixed_event.rb
@@ -5,6 +5,14 @@ class FixedEvent < ApplicationRecord
   validates_with StartBeforeEndValidator
   validates :starts_at, :ends_at, presence: true
   validates :stufe_ids, presence: true
+
   has_and_belongs_to_many :stufen
+
+  has_many_attached :language_documents_de
+  has_many_attached :language_documents_fr
+  has_many_attached :language_documents_it
+
   translates :title, locale_accessors: true, fallbacks: true
+
+  ATTACHMENTS = %i[language_documents_de language_documents_fr language_documents_it].freeze
 end
