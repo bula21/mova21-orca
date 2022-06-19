0459e37d6c0e186366498d6351c1f83301999236
Simon Huber <simon.huber@renuo.ch>
2022-06-19 12:48:24 +0200

    Replace rollbar with sentry

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -7,23 +7,13 @@ module ImportHelper
     save_or_log(record)
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def save_or_log(record)
     return unless record.changed?
     return if record.save
 
     error_message = "Could not save #{record.class.name} with id #{record.id}"
-    if Rollbar.configuration.enabled
-      Rollbar.error(error_message, errors: record.errors.full_messages)
-    else
-      Rails.logger.error "#{error_message}: #{record.errors.full_messages.join(',')}"
-    end
+    ErrorLogger.capture_message(error_message, level: 'warning', extra: { errors: record.errors.full_messages })
   rescue StandardError => e
-    if Rollbar.configuration.enabled
-      Rollbar.error(e, record_class: record.class.name, record_id: record.id)
-    else
-      Rails.logger.error "Unexpected error while saving: #{e}: #{record.class.name} with id #{record.id}"
-    end
+    ErrorLogger.capture_exception(e, extra: { record_class: record.class.name, record_id: record.id })
   end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
