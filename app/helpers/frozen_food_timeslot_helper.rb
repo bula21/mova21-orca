31f3cf16ef40b88bb8574e16617b70558f0b6d14
James <jimmy.levell@outlook.com>
2022-06-29 22:42:48 +0200

    fix: timezone issue

--- app/helpers/frozen_food_timeslot_helper.rb
+++ app/helpers/frozen_food_timeslot_helper.rb
@@ -7,10 +7,7 @@ module FrozenFoodTimeslotHelper
     return I18n.t('units.show.food.parsing_error') unless matches
 
     [matches[1], matches[4]].map do |time|
-      Time.zone = 'Europe/Zurich'
       (Time.zone.parse(time.tr('.', ':')) - 5.hours).strftime('%H.%M')
     end.join('-')
-  rescue StandardError
-    return ''
   end
 end
