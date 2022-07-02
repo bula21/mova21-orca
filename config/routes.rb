839ad382a7ff167c68b1591133e373e5e3ff5537
Diego Steiner <diego.steiner@u041.ch>
2022-07-02 14:33:10 +0000

    fix: rollback reassignment

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   resources :leaders, except: [:destroy]
   resources :unit_activity_executions do
     post :import, on: :collection
-    get :reassign, on: :collection, as: :reassign
+    get :reassign, on: :member, as: :reassign
   end
   resources :activity_executions
   resources :activities do
